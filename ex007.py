5f8e4cdc7607bbfdced918a88cb8ae4d65165e77
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-25 15:15:37 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Digite a pimeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print(f'A sua primeira nota é {n1}. \nA sua segunda nota  é {n2}. \nA sua média é {m}.')
