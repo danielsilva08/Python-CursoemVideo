072d1bd5cea3999e323339e04e40435045f1f624
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-26 11:09:23 -0300

    Update ex012.py

--- ex012.py
+++ ex012.py
@@ -1,1 +1,5 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+valor_produto = float(input('Digite o valor do produto: '))
+preco_comdesconto = valor_produto - (valor_produto* 5 / 100 )  
+print(f'O valor do produto é R$ {valor_produto} com 5% de desconto R$ {preco_comdesconto:.2f}')
