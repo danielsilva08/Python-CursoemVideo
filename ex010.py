1574453aecd94e3bd8d56a993d6310c5157e0a4f
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-26 10:03:00 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,6 +1,7 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+#{:.2f} Ponto fluante 2 casa decimais depois da virgula tipo de formatação
 
-real = float(input('Quanto dinheiro voçe tem na carteira R$? '))
+real = float(input('Quanto dinheiro voçe tem na carteira? R$ '))
 dolar = real / 5.70
-
-print(f'Com R$ {real:.2f} \nvocê pode comprar U$$ {dolar:.2f} dólares.')
+euro = real / 6.15
+print(f'Com R$ {real:.2f} real, você pode comprar U$$ {dolar:.2f} dólares e € {euro:.2f} euros.')
