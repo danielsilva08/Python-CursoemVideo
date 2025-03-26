d7c504b651e817b6533d6b14ea36095e76d7f51a
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-26 15:09:30 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -4,4 +4,4 @@ salario = float(input('Digite o valor do salário: R$ '))
 aumento = float(input('Digite o percentual do aumento do salário: '))
 novo_salario = salario + (salario * aumento / 100)
 
-print(f'O seu salário era R$ {salario:.2f} \nO percentual de aumento foi: {aumento:.0f} % \nO seu novo salário agora é R$ {novo_salario:.2f}')
+print(f'O seu salário era R$ {salario:.2f} \nO percentual de aumento foi: {aumento} % \nO seu novo salário agora é R$ {novo_salario:.2f}')
