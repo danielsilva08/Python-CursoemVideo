37f9956ff5ff42ed414473cf1e7a07f0a372a283
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-26 09:37:19 -0300

    Update ex009.py

--- ex009.py
+++ ex009.py
@@ -2,7 +2,7 @@
 #{:2} para formatar o numero com dois digitos
 
 num = int(input('Digite um numero para ver sua tabuada: '))
-print('=>' * 10)
+print('=>' * 6)
 print(f'{num} x {0:2} = {num*0}')
 print(f'{num} x {1:2} = {num*1}')
 print(f'{num} x {2:2} = {num*2}')
@@ -14,5 +14,5 @@ print(f'{num} x {7:2} = {num*7}')
 print(f'{num} x {8:2} = {num*8}')
 print(f'{num} x {9:2} = {num*9}')
 print(f'{num} x {10:2} = {num*10}')
-print('<=' * 10)
+print('<=' * 6)
 
