6d8fe0917646dd2fa3b539dfb643211b04b9b537
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-27 15:27:23 -0300

    Update ex016.py

--- Ultilizando_modulos/ex016.py
+++ Ultilizando_modulos/ex016.py
@@ -1,1 +1,8 @@
 #Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+#se estivesse somente import math (teria que colocar math.trun(num))
+
+from math import trunc
+num = float(input('Digite um número real: '))
+
+print(f'O valor digitado foi {num} Sua parte inteira é {trunc(num)}')
+
