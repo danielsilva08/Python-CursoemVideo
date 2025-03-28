797d868a2038860bce96c56551e813b77e44a106
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-28 10:20:51 -0300

    Update ex020.py

--- Ultilizando_modulos/ex020.py
+++ Ultilizando_modulos/ex020.py
@@ -3,5 +3,6 @@
 from random import shuffle
 
 lista = ['Daniel', 'Rodrigo', 'Igor', 'Maria']
-escolha = shuffle(lista) #Randon shuffle para embalharar os nomes da lista.
-print(f' O aluno escolhido para apagar a lousa foi ! {escolha}.')
+shuffle(lista) #Randon shuffle para embaralha os nomes da lista.
+
+print(f'A ordem da lista digita è Daniel, Rodrigo, Igor, Maria \nA lista embalharada fica {lista}.')
