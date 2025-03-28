b9480fa784d61a01bb39e813f97e949752254cee
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-28 14:34:55 -0300

    Update ex019.py

--- Ultilizando_modulos/ex019.py
+++ Ultilizando_modulos/ex019.py
@@ -1,7 +1,12 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 
-from random import choice
+import random 
 
-lista = ['Daniel', 'Rodrigo', 'Igor', 'Maria']
-escolha = choice(lista) #Randon choice para ecolher um nome na lista.
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+
+lista = [n1, n2, n3, n4]
+escolha = random.choice(lista) #Randon choice para ecolher um nome na lista.
 print(f' O aluno escolhido para apagar a lousa foi ! {escolha}.')
