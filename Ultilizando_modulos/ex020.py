8d989fe5dfd4c3fff71ffa56562b77333e1bba47
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-28 15:56:52 -0300

    Update ex020.py

--- Ultilizando_modulos/ex020.py
+++ Ultilizando_modulos/ex020.py
@@ -1,4 +1,4 @@
-#
+#O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
 from random import shuffle
 
@@ -8,5 +8,5 @@ n3 = str(input('Terceiro aluno: '))
 n4 = str(input('Quarto aluno: '))
 
 lista = [n1, n2, n3, n4]
-shuffle(lista) #Para mostrar todos da Lista porém em ordem diferente da digitada.
+shuffle(lista) #Para mostrar todos da Lista porém em ordem diferente da digitada. shuffle é embaralhar.
 print(f' O Sortei por orem foi !{lista}.')
