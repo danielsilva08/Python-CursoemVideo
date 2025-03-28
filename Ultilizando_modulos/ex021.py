0b0b3e442371dc94ef089619504261d237709675
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-28 16:38:09 -0300

    atualizei a pasta do local do arquivo

--- Ultilizando_modulos/ex021.py
+++ Ultilizando_modulos/ex021.py
@@ -1,8 +1,8 @@
 #Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 
 import pygame
-pygame.init() #inicia a bilioteca do pygame
-pygame.mixer.music.load('ex021.mp3')
+pygame.init() # inicia o paygame com a biblioteca do pygame
+pygame.mixer.music.load('Ultilizando_modulos\ex021.mp3.mp3') 
 pygame.mixer.music.play()
 input()
-pygame.event.wait #Espera o evento terminar para encerrar o programa ou seja espera a musica terminar.
+pygame.event.wait()#Espera o evento terminar para encerrar o programa
