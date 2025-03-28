1247c25415af34eafa6b2de36a74294646c5e41e
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-28 10:08:14 -0300

    Create ex020.py

---
+++ Ultilizando_modulos/ex020.py
@@ -0,0 +1,7 @@
+
+
+from random import shuffle
+
+lista = ['Daniel', 'Rodrigo', 'Igor', 'Maria']
+escolha = shuffle(lista) #Randon shuffle para embalharar os nomes da lista.
+print(f' O aluno escolhido para apagar a lousa foi ! {escolha}.')
