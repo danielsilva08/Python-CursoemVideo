b328f05548510c574566da517883f477192aef3a
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-28 14:45:17 -0300

    Update ex020.py

--- Ultilizando_modulos/ex020.py
+++ Ultilizando_modulos/ex020.py
@@ -1,8 +1,12 @@
-
+#
 
 from random import shuffle
 
-lista = ['Daniel', 'Rodrigo', 'Igor', 'Maria']
-shuffle(lista) #Randon shuffle para embaralha os nomes da lista.
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
 
-print(f'A ordem da lista digita è Daniel, Rodrigo, Igor, Maria \nA lista embalharada fica {lista}.')
+lista = [n1, n2, n3, n4]
+shuffle(lista) #Para mostrar todos da Lista porém em ordem diferente da digitada.
+print(f' O Sortei por orem foi !{lista}.')
