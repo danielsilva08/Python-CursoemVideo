13b2c911a249167396c8867a60d1e35d3cdcd950
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-31 09:46:30 -0300

    criei ex09f e atuliazei alguns nomes

--- Manipulando_textos/aula09e.py
+++ Manipulando_textos/aula09e.py
@@ -1,4 +1,4 @@
 frase = 'curso em video python'
 print(frase.replace('python', 'android'))
 print(frase)
-#replace troquei o python da frase pro android
+#replace troquei o python da frase por android
---
+++ Manipulando_textos/aula09f.py
@@ -0,0 +1,2 @@
+frase = 'curso em video python'
+print('curso' in frase) # A palavra curso está dentro da frase?
