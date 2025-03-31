325d08a1ebedad7a2058cd212db91a2c39e8a45e
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-31 09:57:40 -0300

    Create aula09i.py

---
+++ Manipulando_textos/aula09i.py
@@ -0,0 +1,11 @@
+frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido[0])
+
+#mostra somente a palavra curso que está na posição 0
+#o primeiro item da lista
+
+rase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido[2] [3])
+#detro do divido 2 que é video mostre somente a letra 3 que no caso é a letra e.
