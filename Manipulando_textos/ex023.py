8bbc16c1eb43693d2c19dce84b0f2e944d211123
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-31 14:47:56 -0300

    Update ex023.py

--- Manipulando_textos/ex023.py
+++ Manipulando_textos/ex023.py
@@ -2,3 +2,13 @@
 #informe o número
 #analisando o numero tal.
 #unidade dezena centenda milhar
+
+num = int(input('Infome o número 0 a 9999: '))
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+
+
+print(f'Analizando o número {num}')
+print(f'Unidade{[unidade]} Dezena{[dezena]} Centena{[centena]} Milhar{[milhar]}')
