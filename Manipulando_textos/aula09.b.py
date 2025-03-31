f7cff30bc35526d059ab1556a6a4aed88b80211a
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-31 09:33:08 -0300

    Create aula09.b.py

---
+++ Manipulando_textos/aula09.b.py
@@ -0,0 +1,4 @@
+#Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+
+frase = ('curso em video')
+print(frase.count('o')) #count conta quantas letras tem na frase. No caso pedi a letra o
