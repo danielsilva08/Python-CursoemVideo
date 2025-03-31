8e15854a3523b1eb6d0712e75e9920290a20036e
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-31 09:31:25 -0300

    Update aula09a.py

--- Manipulando_textos/aula09a.py
+++ Manipulando_textos/aula09a.py
@@ -1,2 +1,4 @@
 #Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
 
+frase = ('curso em video')
+print(frase[3])
