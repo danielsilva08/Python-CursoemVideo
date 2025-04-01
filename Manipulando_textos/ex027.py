67ea48d9d5acb54dce945e4f1890207c572b5a36
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-01 15:16:51 -0300

    Update ex027.py

--- Manipulando_textos/ex027.py
+++ Manipulando_textos/ex027.py
@@ -1,1 +1,4 @@
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print(f'Prazer em te conhecer.\nSeu primeiro nome é {nome[0]}\nSeu último nome é {nome[len(nome)-1]}')
