b93bf598e3aa137ba0b16710b737fa3b722c2285
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-01 15:49:32 -0300

    Update aula10a.py

--- condiçoes(if..else)/aula10a.py
+++ condiçoes(if..else)/aula10a.py
@@ -1,1 +1,7 @@
-#vamos aprender como utilizar estruturas condcionais simples e compostas nos seus #programas em Python.
+#vamos aprender como utilizar estruturas condicionais simples e compostas nos seus #programas em Python. Condições simples e compostas.
+nome = str(input('Qual o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome lindo!')
+else:
+    print('Que nome normal!')
+print(f'Bom dia, {nome}.')        
