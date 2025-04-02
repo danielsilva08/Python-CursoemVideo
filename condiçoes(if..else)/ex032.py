be5572fc2257bc4e6645b526e7538956343a5a17
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-02 09:44:57 -0300

    Update ex032.py

--- condiçoes(if..else)/ex032.py
+++ condiçoes(if..else)/ex032.py
@@ -5,8 +5,8 @@ ano = int(input('Que ano quer analisar? coloque 0 analisar o ano atual: '))
 if ano == 0:
     ano = date.today().year # Pega o ano atual da maquina
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-# se o ano divisivel por 4 e não pode ser divisivel por 100 dif 0
-# ou em tão o ano ser divisivel por 400 
+# ano TEM QUE SER divisivel por 4 IGUAL A 0 e TAMBÉM não pode ser divisivel por 100 DIfERENTE DE 0
+# ou em tão o ano ser divisivel por 400 IGUALA 0
      print(f'O ano {ano} é BISSEXTO.')       
 else:
      print(f'O ano {ano} não é BISSEXTO.') 
