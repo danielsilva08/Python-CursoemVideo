c0786645846cc1341145f0b6904c457b9b0fd4c0
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-02 09:38:21 -0300

    Update ex032.py

--- condiçoes(if..else)/ex032.py
+++ condiçoes(if..else)/ex032.py
@@ -1,1 +1,12 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+from datetime import date
+
+ano = int(input('Que ano quer analisar? coloque 0 analisar o ano atual: '))
+if ano == 0:
+    ano = date.today().year # Pega o ano atual da maquina
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+# se o ano divisivel por 4 e não pode ser divisivel por 100 dif 0
+# ou em tão o ano ser divisivel por 400 
+     print(f'O ano {ano} é BISSEXTO.')       
+else:
+     print(f'O ano {ano} não é BISSEXTO.') 
