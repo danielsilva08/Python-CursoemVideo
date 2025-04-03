ce5d51dd947f0fd500242e565edf5625b7904912
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-03 10:58:56 -0300

    Create ex035.py

---
+++ condiçoes(if..else)/ex035.py
@@ -0,0 +1,13 @@
+ #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print('-=' * 12)
+print('Analisador de Triângulos')
+print('-=' * 12)
+
+reta1 = float(input('Primeiro Segmento: '))
+reta2 = float(input('Primeiro Segmento: '))
+reta3 = float(input('Primeiro Segmento: '))
+
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    print(f'Os segmentos acima PODEM FORMA UM TRIÂNGULO.')
+else:
+    print(f'Os segmentos acima NÃO PODEM FORMAR UM TRIÂNGULO.')        
