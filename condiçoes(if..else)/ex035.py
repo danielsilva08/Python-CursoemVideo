bfcfd72546e9cdf1180891bae07911121413c5ba
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-10 09:48:41 -0300

    Update ex035.py

--- condiçoes(if..else)/ex035.py
+++ condiçoes(if..else)/ex035.py
@@ -4,8 +4,8 @@ print('Analisador de Triângulos')
 print('-=' * 12)
 
 reta1 = float(input('Primeiro Segmento: '))
-reta2 = float(input('Primeiro Segmento: '))
-reta3 = float(input('Primeiro Segmento: '))
+reta2 = float(input('Segundo Segmento: '))
+reta3 = float(input('Terceiro Segmento: '))
 
 if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
     print(f'Os segmentos acima PODEM FORMA UM TRIÂNGULO.')
