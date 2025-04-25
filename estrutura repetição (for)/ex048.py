951ecd0fe2e359c9d0323c68c04f6d95005e71ac
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-25 09:45:12 -0300

    Update ex048.py

--- estrutura repetição (for)/ex048.py
+++ estrutura repetição (for)/ex048.py
@@ -3,6 +3,6 @@ soma = 0
 cont = 0
 for c in range(1, 501, 2):
     if c % 3  == 0:
-        soma = soma + c
-        cont = cont + 1
+        soma = soma + c #acumulador soma ou multiplica um valor
+        cont = cont + 1 #contador soma + 1
 print(f'A soma de todos os {cont} valores solicitados é {soma} ')
