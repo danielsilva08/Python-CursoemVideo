e5400387185645e54ece19abf7a577a99f5ab2a1
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-25 10:19:36 -0300

    Update ex049.py

--- estrutura repetição (for)/ex049.py
+++ estrutura repetição (for)/ex049.py
@@ -1,5 +1,5 @@
 #Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
 numero = int(input('Digite um número para ver a sua tabuada: '))
-for c in range(1, 11):
+for c in range(0, 11):
     soma = numero * c
     print(f'{numero} x {c} = {soma} ')
