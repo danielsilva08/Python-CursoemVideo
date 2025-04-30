9156cc7efaf63678c03e47dfd45c565a9201d5df
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-30 09:34:31 -0300

    Create aula 14d.py

---
+++ estrut repetição (while)/aula 14d.py
@@ -0,0 +1,11 @@
+numero = 1
+par = 0
+impar = 0
+while numero != 0:
+    numero = int(input('Digite um valor: '))
+    if numero != 0: # para desconsiderar o numero par 0 na contagem.
+        if numero % 2 == 0:
+            par = par + 1
+        else:
+            impar = impar + 1
+print(f'você digitou {par} numeros pares e {impar} numeros impares.')
