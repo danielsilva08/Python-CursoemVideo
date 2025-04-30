b79ef6cc3255b8f29f62786be8bb11f1a5f4a6c8
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-30 09:36:46 -0300

    Update aula 14d.py

--- estrut repetição (while)/aula 14d.py
+++ estrut repetição (while)/aula 14d.py
@@ -3,7 +3,7 @@ par = 0
 impar = 0
 while numero != 0:
     numero = int(input('Digite um valor: '))
-    if numero != 0: # para desconsiderar o numero par 0 na contagem.
+    if numero != 0: # para desconsiderar o numero nulo 0 na contagem.
         if numero % 2 == 0:
             par = par + 1
         else:
