68df7d049800bd0537048403a66cae45e4eabd04
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-30 09:34:36 -0300

    Create aula 14c.py

---
+++ estrut repetição (while)/aula 14c.py
@@ -0,0 +1,5 @@
+resposta = 'S'
+while resposta == 'S':
+    numero = int(input('Digite um número: '))
+    resposta = str(input('Quer continuar [S/N] ')).upper() #UPPER PARA LER MINUSCULA TAMBÉM
+print('FIM')    
