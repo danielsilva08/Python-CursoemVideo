842552d9274cea568f9ef95d6435b5779d1db0dc
Daniel Silva <daniel_silva008@hotmail.com>
2025-04-30 10:52:13 -0300

    Update ex057.py

--- estrut repetição (while)/ex057.py
+++ estrut repetição (while)/ex057.py
@@ -1,9 +1,9 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 sexo = 'M/F'
 while sexo:
-    sexo = str(input('Informe o sexo[M/F]: ')).upper()
+    sexo = str(input('Informe o sexo[M/F]: ')).strip().upper()[0]
     if sexo != 'M' and 'F':
-        print('Você digitou errado. Por favor, Digite o sexo novamente.')
+        print('Dados invalidos. Por favor, Digite o sexo novamente.')
         
    
       
