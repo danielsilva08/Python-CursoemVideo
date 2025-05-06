537dea829d0043e33f173308d14e4e76e3ccb87a
Daniel Silva <daniel_silva008@hotmail.com>
2025-05-06 18:54:27 -0300

    Update ex061.py

--- estrut repetição (while)/ex061.py
+++ estrut repetição (while)/ex061.py
@@ -1,9 +1,15 @@
 #lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
 print('Gerador de PA')
-print(end='=-=' * 10 )
+print(end='=-='*10)
+
 primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
-decimo = 10
-while primeiro > decimo:
-    print(f'{primeiro}')
-    primeiro = primeiro + razao
+termo = primeiro
+contador = 1
+while contador <= 10:
+    print(f'{termo} ->', end='')
+    termo += razao
+    contador += 1
+
+
+
