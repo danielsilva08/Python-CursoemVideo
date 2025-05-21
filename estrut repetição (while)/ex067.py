fe9fc26791616b58344c1b5f5f3929e2f4535e03
Daniel Silva <daniel_silva008@hotmail.com>
2025-05-21 17:35:03 -0300

    Update ex067.py

---
+++ estrut repetição (while)/ex067.py
@@ -0,0 +1,12 @@
+#Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+while True:
+    numero = int(input('Quer ver a tabuada de qual valor? '))
+    for c in range(0, 11):
+        razao = numero * c
+        print(f'{numero} x {c} = {razao}')
+    if numero < 0:
+        break
+print('Encerrando') 
+
+   
+    
