a085dfd7afeb20947825f970574bc48cffe997af
Daniel Silva <daniel_silva008@hotmail.com>
2025-05-21 16:50:49 -0300

    Update ex066.py

--- estrut repetição (while)/ex066.py
+++ estrut repetição (while)/ex066.py
@@ -1,7 +1,7 @@
 #Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 numero = soma = digitados = 0
 while True:
-    numero = int(input('Digite o número: '))
+    numero = int(input('Digite um número: [999 para parar] '))
     if numero == 999:
         break
     soma += numero
