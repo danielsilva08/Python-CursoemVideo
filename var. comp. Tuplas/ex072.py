13859b7fadeaf6c1012903dd56691daa6ebdf43c
Daniel Silva <daniel_silva008@hotmail.com>
2026-06-23 11:20:33 -0300

    corrigi ex072

--- var. comp. Tuplas/ex072.py
+++ var. comp. Tuplas/ex072.py
@@ -1,7 +1,8 @@
 cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco')
 
 while True:
-    num = int(input('Digite um numero:'))
+    num = int(input('Digite um numero de 0 a 5:'))
     if 0 <= num <= 5: 
         break 
-    print(f' Voce digitou o numero {cont[num]}')
+    print('Numero invalido. Tente novamente.', end=' ')
+print(f' Voce digitou o numero {cont[num]}')
