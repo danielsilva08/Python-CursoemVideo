0c0cd3c0abbcfd1d04e3a820ac51468e635720d0
Daniel Silva <daniel_silva008@hotmail.com>
2026-06-24 09:22:43 -0300

    Create ex073.py

---
+++ var. comp. Tuplas/ex073.py
@@ -0,0 +1,8 @@
+
+
+times = ('flamengo', 'vasco', 'palmeiras', 'santos', 'corinthians', 'fluminense', 'botafogo', 'gremio', 'internacional', 'atletico', 'cruzeiro', 'sao paulo', 'bahia', 'sport', 'ceara', 'fortaleza', 'goias', 'avai', 'chapecoense', 'juventude')
+print(times)
+print('-=' * 50)
+print(f'O quinto time é: {times[4]}')
+print('-=' * 50)
+print(f'Os 5 primeiros times são: {times[0:5]}')
