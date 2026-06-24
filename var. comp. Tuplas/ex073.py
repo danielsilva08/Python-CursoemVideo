971943439359e7044f522b7bcf9a6d74ce3239a6
Daniel Silva <daniel_silva008@hotmail.com>
2026-06-24 10:33:54 -0300

    Update ex073.py

--- var. comp. Tuplas/ex073.py
+++ var. comp. Tuplas/ex073.py
@@ -1,4 +1,9 @@
+#Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: 
 
+#a) Os 5 primeiros times.
+#b) Os últimos 4 colocados.
+#c) Times em ordem alfabética.
+#d) Em que posição está o time da Chapecoense.
 
 times = ('flamengo', 'vasco', 'palmeiras', 'santos', 'corinthians', 'fluminense', 'botafogo', 'gremio', 'internacional', 'atletico', 'cruzeiro', 'sao paulo', 'bahia', 'sport', 'ceara', 'fortaleza', 'goias', 'avai', 'chapecoense', 'juventude')
 print(times)
